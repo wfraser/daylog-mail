9c7ce3dd471701d5a4043b2a0495f646db39ca46
Bill Fraser <wfraser@codewise.org>
2019-10-09 21:17:11 -0700

    let's try sendmail instead

--- src/send.rs
+++ src/send.rs
@@ -1,7 +1,7 @@
 use crate::{SendArgs, todays_date};
 use crate::message_id::{self, read_secret_key};
 use failure::ResultExt;
-use std::io::Write;
+use std::io::{self, Write};
 use std::process::{Command, Stdio};
 
 pub fn send(args: SendArgs) -> Result<(), failure::Error> {
@@ -14,29 +14,39 @@ pub fn send(args: SendArgs) -> Result<(), failure::Error> {
     let msgid = message_id::gen_message_id(&args.username, &today, key_bytes)
         .with_context(|e| format!("failed to generate message ID: {}", e))?;
 
-    let mut child = Command::new("mail")
-        .arg("-C")
-        .arg(format!("Message-ID: <{}>", msgid))
-        .arg("-r")
-        .arg(args.return_addr)
-        .arg("-s")
-        .arg(format!("Daylog for {}", today))
-        .arg("-.")
-        .arg(args.email)
+    let mut child = Command::new("sendmail")
+        .arg("-i")
+        .arg("-f")
+        .arg(&args.return_addr)
+        .arg(&args.email)
         .stdin(Stdio::piped())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .spawn()
-        .with_context(|e| format!("failed to run 'mail' command: {}", e))?;
+        .with_context(|e| format!("failed to run 'sendmail' command: {}", e))?;
 
     {
-        let stdin = child.stdin.as_mut().expect("failed to get 'mail' command stdin");
-        write!(stdin, "What'd you do today, {}?\r\n\r\n-- \r\nsent by daylog\r\n", today)
+        let stdin = child.stdin.as_mut().expect("failed to get 'sendmail' command stdin");
+        write_email(stdin, &args, &today, &msgid)
             .with_context(|e| format!("failed to write email: {}", e))?;
     }
 
     child.wait()
         .with_context(|e| format!("failed to wait for 'mail' command: {}", e))?;
 
+    Ok(())
+}
+
+fn write_email(mut w: impl Write, args: &SendArgs, today: &str, msgid: &str) -> io::Result<()> {
+    write!(w, "Date: {}\r\n", chrono::Utc::now().to_rfc2822())?;
+    write!(w, "Subject: Daylog for {}\r\n", today)?;
+    write!(w, "Sender: <{}>\r\n", args.return_addr)?;
+    write!(w, "To: <{}>\r\n", args.email)?;
+    write!(w, "Message-ID: <{}>\r\n", msgid)?;
+    write!(w, "\r\n")?;
+    write!(w, "What'd you do today, {}?\r\n", today)?;
+    write!(w, "\r\n")?;
+    write!(w, "-- \r\n")?;
+    write!(w, "sent by daylog\r\n")?;
     Ok(())
 }

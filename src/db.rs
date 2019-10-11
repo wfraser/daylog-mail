452f0a8719236f79537435b22556cc9b99427159
William R. Fraser <wfraser@codewise.org>
2019-10-11 02:03:34 -0700

    holy crap, don't overwrite everything! 😱

--- src/db.rs
+++ src/db.rs
@@ -43,13 +43,16 @@ impl Database {
             code: rusqlite::ErrorCode::ConstraintViolation,
             extended_code: 2067, // SQLITE_CONSTRAINT_UNIQUE
         }, _msg)) = insert_result {
-            let mut update_body: String = tx.query_row_named(
-                "SELECT body FROM entries WHERE username = :username AND date = :date",
+            let (id, mut update_body): (i64, String) = tx.query_row_named(
+                "SELECT id, body FROM entries WHERE username = :username AND date = :date",
                 rusqlite::named_params!{":username": username, ":date": date},
-                |row| row.get(0))?;
+                |row| Ok((row.get(1)?, row.get(0)?)))?;
+            eprintln!("updating existing row {}: {}/{}", id, username, date);
             update_body.push('\n');
             update_body +=  body;
-            tx.execute_named("UPDATE entries SET body = :body", named_params!{":body": update_body})
+            tx.execute_named(
+                "UPDATE entries SET body = :body WHERE id = :id",
+                named_params!{":body": update_body, ":id": id})
                 .context("failed to update existing entry")?;
         } else {
             insert_result.with_context(|e| format!("failed to insert entry: {}", e))?;

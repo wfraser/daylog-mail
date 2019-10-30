209cf1bf2c6c4f9360e9f58cad360a9230fbee29
William R. Fraser <wfraser@codewise.org>
2019-10-30 00:08:29 -0700

    cargo update
    also incorporate my contribution to the maildir crate :)

--- src/maildir.rs
+++ src/maildir.rs
@@ -1,6 +1,5 @@
 use crate::mail::{Mail, MailProcessAction, MailSource, RunStats};
 use failure::{Error, ResultExt};
-use std::fs;
 use maildir::Maildir;
 use std::path::Path;
 
@@ -43,22 +42,10 @@ impl MailSource for DaylogMaildir {
             };
             match action {
                 MailProcessAction::Remove => {
-                    let path = entry.path();
-
-                    // for now, let's save them as seen
-                    // TODO: this should probably be a feature in the maildir crate.
-                    let mut filename = path.file_name().unwrap().to_owned();
-                    filename.push(":2,S");
-
-                    let mut new = path.to_owned();
-                    new.pop();
-                    new.pop();
-                    new.push("cur");
-                    new.push(filename);
-
-                    //fs::remove_file(path)
-                    fs::rename(path, &new)
-                        .with_context(|e| format!("failed to remove message {:?}: {}", path, e))?;
+                    //self.maildir.delete(entry.id())
+                    // for now, let's save them as seen instead.
+                    self.maildir.move_new_to_cur_with_flags(entry.id(), "S")
+                        .with_context(|e| format!("failed to remove message {:?}: {}", entry.id(), e))?;
                     stats.num_removed += 1;
                 }
                 MailProcessAction::Keep => {
